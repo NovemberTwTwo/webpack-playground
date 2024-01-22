a55bc3c8d3bb12acb82af1fab6fb57a87ed73e34
DaHyunOhe <peelsob4d@gmail.com>
2024-01-22 20:19:29 +0900

    TEST : Testing csv/xml loading case

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Animation from './Animation.gif';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -9,6 +11,10 @@ function component() {
   const myAnim = new Image();
   myAnim.src = Animation;
   element.appendChild(myAnim);
+
+  console.log(Data);
+  console.log(Notes);
+
   return element;
 }
 
