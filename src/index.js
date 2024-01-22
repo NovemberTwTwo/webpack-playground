1702e1b7de4fa9d5231c31c1523e5645226c4ec7
DaHyunOhe <peelsob4d@gmail.com>
2024-01-22 19:43:53 +0900

    TEST : Testing css bundling with style/css loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = ['Hello', 'webpack'].join(' ');
+  element.classList.add('hello');
   return element;
 }
 
