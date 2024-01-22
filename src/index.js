b6efdee46390a9dce06820ddbaf6fa48f9dd796d
DaHyunOhe <peelsob4d@gmail.com>
2024-01-22 20:03:49 +0900

    TEST : Image load test with asset/resource

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 import './style.css';
+import Animation from './Animation.gif';
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = ['Hello', 'webpack'].join(' ');
   element.classList.add('hello');
+  const myAnim = new Image();
+  myAnim.src = Animation;
+  element.appendChild(myAnim);
   return element;
 }
 
